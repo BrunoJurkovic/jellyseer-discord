62b80330cc1bf822ce2a6f392c2aad8d2924eb27
Bruno Jurković <bruno.jurkovic1@gmail.com>
2023-02-26 17:59:02 +0100

    :sparkles: Initialized the project

---
+++ lib/jellyseer_discord.dart
@@ -0,0 +1,10 @@
+/// jellyseer_discord, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate jellyseer_discord
+/// dart pub global activate jellyseer_discord
+///
+/// # see usage
+/// jellyseer_discord --help
+/// ```
+library jellyseer_discord;
